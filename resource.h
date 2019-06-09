25c192bebdc4f90267512a36d11a618e85926ce0
YangPatrick <patrick.yangyz@gmail.com>
2019-06-09 18:30:15 +0800

    add music game controller

--- resource.h
+++ resource.h
@@ -2,13 +2,38 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 DirextX2DDemo.rc 使用
 //
-#define IDB_PNG2                        104
+#define IDB_PNG1                        106
+#define IDB_PNG2                        107
+#define IDB_PNG3                        108
+#define IDB_PNG4                        109
+#define IDB_PNG5                        110
+#define IDB_PNG6                        111
+#define IDB_PNG7                        112
+#define IDB_PNG8                        113
+#define IDB_PNG9                        114
+#define IDB_PNG10                       115
+#define IDB_PNG11                       116
+#define IDB_PNG12                       117
+#define IDB_PNG13                       118
+#define IDB_PNG14                       119
+#define IDB_PNG15                       120
+#define IDB_PNG16                       121
+#define IDB_PNG17                       122
+#define IDB_PNG18                       123
+#define IDB_PNG19                       124
+#define IDB_PNG20                       125
+#define IDB_PNG21                       126
+#define IDB_PNG22                       127
+#define IDB_PNG23                       128
+#define IDB_PNG24                       129
+#define IDB_PNG25                       130
+#define IDB_PNG26                       131
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
--- src/GameController.cpp
+++ src/GameController.cpp
@@ -11,29 +11,76 @@
 #include "StrokeRoundedRectangle.h"
 #include "GameLine.h"
 #include "GameText.h"
+#include <time.h>
+#include <stdlib.h>
 
 using namespace DirectX;
 
+const int base_site = 106;
+const int Max_Sequence = 100;
+const int Maxn = 5;
+const int SPEED = 1000;
+const int EFFECT_AREA = 80;
+const int SCROE_SITE = 600;
 
-GameRectangle* temRect;
-StrokeRectangle* temRect2;
-GameCircle* temCircle;
-BitmapRectangle* temRect3;
+typedef struct NODE
+{
+	BitmapRectangle* shape;
+	int value;   //  0:up  1:down  2:left  3:right
+}Node;
+
+
 GirdBackground* background;
-RoundedRectangle* temRect4;
-StrokeRoundedRectangle* temRect5;
-GameLine* line;
 GameText* gameText;
 GameSound* sound;
 
-std::wstring textStr = L"Use W,A,S,D to move.";
+std::wstring textStr = L"Use W,A,S,D to catch the block.";
 
 template <class T>
 using ComPtr = Microsoft::WRL::ComPtr<T>;
-ComPtr<ID2D1Bitmap> temBitmap;
 ComPtr<ID2D1SolidColorBrush> m_pLightSlateGrayBrush;
 ComPtr<ID2D1SolidColorBrush> m_pCornflowerBlueBrush;
 ComPtr<IDWriteTextFormat> defaultTextFormat;
+
+ComPtr<ID2D1Bitmap> toLeft[Maxn], toRight[Maxn], toUp[Maxn], toDown[Maxn];
+ComPtr<ID2D1Bitmap> toLeftsite, toRightsite, toUpsite, toDownsite;
+ComPtr<ID2D1Bitmap> roomBitmap, endGameBitmap;
+
+BitmapRectangle* LeftArrow[Maxn];
+BitmapRectangle* RightArrow[Maxn];
+BitmapRectangle* UpArrow[Maxn];
+BitmapRectangle* DownArrow[Maxn];
+
+BitmapRectangle* Leftsite;
+BitmapRectangle* Rightsite;
+BitmapRectangle* Upsite;
+BitmapRectangle* Downsite;
+
+BitmapRectangle* roomRectangle;
+BitmapRectangle* endGameRectangle;
+bool flag = true;
+
+time_t start, end;
+int site = 0;
+
+Node Sequence[Max_Sequence];   // 0：上  1：下  2：左  3：右
+int s_site = 0;
+int now_site = 0;
+
+int score = 0;
+int life = 100;
+
+GameText* scoreText;
+GameText* lifeText;
+GameText* showScore;
+GameText* showLife;
+
+std::wstring scoreStr = L"Your score: ";
+std::wstring showScoreStr = std::to_wstring(score);
+std::wstring lifeStr = L"Your life: ";
+std::wstring showLifeStr = std::to_wstring(life);
+
+
 
 GameController::GameController(HINSTANCE hInstance)
 	: GameApp(hInstance)
@@ -47,57 +94,118 @@ GameController::GameController(HINSTANCE hInstance, int windowWidth, int windowH
 
 GameController::~GameController()
 {
-	delete temRect;
-	delete temRect2;
-	delete temRect3;
-	delete temCircle;
-	delete temRect4;
-	delete temRect5;
-	delete line;
-	temBitmap.Reset();
 	m_pLightSlateGrayBrush.Reset();
 	m_pCornflowerBlueBrush.Reset();
 	defaultTextFormat.Reset();
+
+	for (int i = 0; i < Maxn; i++)
+	{
+		delete LeftArrow[i];
+		delete RightArrow[i];
+		delete UpArrow[i];
+		delete DownArrow[i];
+
+		toLeft[i].Reset();
+		toRight[i].Reset();
+		toUp[i].Reset();
+		toDown[i].Reset();
+	}
+
+	delete Leftsite;
+	delete Rightsite;
+	delete Upsite;
+	delete Downsite;
+
+	toLeftsite.Reset();
+	toRightsite.Reset();
+	toUpsite.Reset();
+	toDownsite.Reset();
 }
 
 void GameController::GameInit()
 {
+	start = time(NULL);
+	site = 0;
+	s_site = 0;
+	now_site = 0;
+	score = 0;
+	life = 100;
+	srand((unsigned)time(NULL));
+
+	LoadResourceBitmap(MAKEINTRESOURCE(IDB_PNG24), L"PNG", 0, 0, toLeftsite.GetAddressOf());
+	LoadResourceBitmap(MAKEINTRESOURCE(IDB_PNG23), L"PNG", 0, 0, toRightsite.GetAddressOf());
+	LoadResourceBitmap(MAKEINTRESOURCE(IDB_PNG21), L"PNG", 0, 0, toUpsite.GetAddressOf());
+	LoadResourceBitmap(MAKEINTRESOURCE(IDB_PNG22), L"PNG", 0, 0, toDownsite.GetAddressOf());
+	LoadResourceBitmap(MAKEINTRESOURCE(IDB_PNG25), L"PNG", 0, 0, roomBitmap.GetAddressOf());
+	LoadResourceBitmap(MAKEINTRESOURCE(IDB_PNG26), L"PNG", 0, 0, endGameBitmap.GetAddressOf());
+
+	for (int i = 0; i < Maxn; i++)
+	{
+		LoadResourceBitmap(MAKEINTRESOURCE(i + base_site), L"PNG", 0, 0, toLeft[i].GetAddressOf());
+		LoadResourceBitmap(MAKEINTRESOURCE(i + base_site + 5), L"PNG", 0, 0, toRight[i].GetAddressOf());
+		LoadResourceBitmap(MAKEINTRESOURCE(i + base_site + 10), L"PNG", 0, 0, toUp[i].GetAddressOf());
+		LoadResourceBitmap(MAKEINTRESOURCE(i + base_site + 15), L"PNG", 0, 0, toDown[i].GetAddressOf());
+	}
+
 	//例子
-	LoadResourceBitmap(MAKEINTRESOURCE(IDB_PNG2), L"PNG", 0, 0, temBitmap.GetAddressOf());
 	CreateSolidColorBrush(m_pLightSlateGrayBrush.GetAddressOf(), D2D1::ColorF(D2D1::ColorF::LightSlateGray));
 	CreateSolidColorBrush(m_pCornflowerBlueBrush.GetAddressOf(), D2D1::ColorF(D2D1::ColorF::CornflowerBlue));
 	CreateTextFormat(defaultTextFormat.GetAddressOf(), 30.f, L"Microsoft YaHei");
 
 	//LoadBitmapFromFile(L"resources/head.jpg", 0, 0, temBitmap.GetAddressOf());
 	background = new GirdBackground(m_pLightSlateGrayBrush.Get());
-	temRect = new GameRectangle(m_pLightSlateGrayBrush.Get(), 100.f, 100.f, 50.f, 80.f, 30.f);
-	temRect2 = new StrokeRectangle(m_pCornflowerBlueBrush.Get(), 400.f, 300.f, 70.f, 30.f, 5.0f);
-	temCircle = new GameCircle(m_pCornflowerBlueBrush.Get(), 600.f, 400.f, 40.f);
-	temRect4 = new RoundedRectangle(m_pLightSlateGrayBrush.Get(), 300.f, 500.f, 120.f, 50.f, 10.f);
-	temRect5 = new StrokeRoundedRectangle(m_pLightSlateGrayBrush.Get(), 500.f, 500.f, 120.f, 50.f, 3.f, 10.f);
-	line = new GameLine(m_pCornflowerBlueBrush.Get(), 300.f, 300.f, 600.f, 900.f, 3.f);
-
-	
 
 
 	gameText = new GameText(m_pCornflowerBlueBrush.Get(), defaultTextFormat.Get(), textStr.c_str(), textStr.size(), 0, 0, 1200, 100);
+	scoreText = new GameText(m_pCornflowerBlueBrush.Get(), defaultTextFormat.Get(), scoreStr.c_str(), scoreStr.size(), 0, 0, 600, 300);
+	showScore = new GameText(m_pCornflowerBlueBrush.Get(), defaultTextFormat.Get(), showScoreStr.c_str(), showScoreStr.size(), 0, 0, 1000, 300);
+	lifeText = new GameText(m_pCornflowerBlueBrush.Get(), defaultTextFormat.Get(), lifeStr.c_str(), lifeStr.size(), 0, 0, 600, 500);
+	showLife = new GameText(m_pCornflowerBlueBrush.Get(), defaultTextFormat.Get(), showLifeStr.c_str(), showLifeStr.size(), 0, 0, 1000, 500);
 
-	temRect3 = new BitmapRectangle(temBitmap.Get(), ((temBitmap)->GetSize()).width, ((temBitmap)->GetSize()).height);
-	
+	Leftsite = new BitmapRectangle(toLeftsite.Get(), ((toLeftsite)->GetSize()).width, ((toLeftsite)->GetSize()).height);
+	Rightsite = new BitmapRectangle(toRightsite.Get(), ((toRightsite)->GetSize()).width, ((toRightsite)->GetSize()).height);
+	Upsite = new BitmapRectangle(toUpsite.Get(), ((toUpsite)->GetSize()).width, ((toUpsite)->GetSize()).height);
+	Downsite = new BitmapRectangle(toDownsite.Get(), ((toDownsite)->GetSize()).width, ((toDownsite)->GetSize()).height);
+
+	roomRectangle = new BitmapRectangle(roomBitmap.Get(), 1500, 800);
+	endGameRectangle = new BitmapRectangle(endGameBitmap.Get(), ((endGameBitmap)->GetSize()).width, ((endGameBitmap)->GetSize()).height);
+
+	Leftsite->setX(300);
+	Leftsite->setY(SCROE_SITE);
+
+	Rightsite->setX(700);
+	Rightsite->setY(SCROE_SITE);
+
+	Upsite->setX(500);
+	Upsite->setY(SCROE_SITE);
+
+	Downsite->setX(900);
+	Downsite->setY(SCROE_SITE);
+
+	for (int i = 0; i < Maxn; i++)
+	{
+		LeftArrow[i] = new BitmapRectangle(toLeft[i].Get(), ((toLeft[i])->GetSize()).width, ((toLeft[i])->GetSize()).height);
+		RightArrow[i] = new BitmapRectangle(toRight[i].Get(), ((toRight[i])->GetSize()).width, ((toRight[i])->GetSize()).height);
+		UpArrow[i] = new BitmapRectangle(toUp[i].Get(), ((toUp[i])->GetSize()).width, ((toUp[i])->GetSize()).height);
+		DownArrow[i] = new BitmapRectangle(toDown[i].Get(), ((toDown[i])->GetSize()).width, ((toDown[i])->GetSize()).height);
+	}
+
+
 	addGraphic(background);
+	addGraphic(roomRectangle);
 	addGraphic(gameText);
-	addGraphic(temRect3);
-	addGraphic(temRect);
-	addGraphic(temRect2);
-	addGraphic(temRect4);
-	addGraphic(temRect5);
-	addGraphic(temCircle);
-	//addGraphic(line);
-	
+	addGraphic(scoreText);
+	addGraphic(lifeText);
+	addGraphic(showScore);
+	addGraphic(showLife);
+
+	addGraphic(Leftsite);
+	addGraphic(Rightsite);
+	addGraphic(Upsite);
+	addGraphic(Downsite);
 
 	soundAdapter.createSound((LPWSTR)L"resources/M11-07-27.wav", &sound, 10);
 	sound->play();
-	sound->stop();
 }
 
 
@@ -112,40 +220,181 @@ void GameController::UpdateScene(float dt)
 
 	// 更新鼠标按钮状态跟踪器
 	m_MouseTracker.Update(mouseState);
-	
-	int x = 1;
-	if(m_KeyboardTracker.lastState.IsKeyUp(Keyboard::W) && keyState.IsKeyDown(Keyboard::W)) {
-		
-		x++;
-	}
-	if (x == 10) {
-		x++;
-	}
-	// 例子
-	// 鼠标拖动
-	if (mouseState.leftButton == true && m_MouseTracker.leftButton == m_MouseTracker.HELD) {
-		
-	}
 
-	if (mouseState.x > temRect3->getX() && mouseState.x < temRect3->getX() + temRect3->getWidth() && mouseState.y > temRect3->getY() && mouseState.y < temRect3->getY() + temRect3->getHeight()) {
-		temRect3->setOpacity(0.5f);
+	if (flag == true) {
+		showScoreStr = std::to_wstring(score);
+		showScore->setText(showScoreStr.c_str(), showScoreStr.size());
+
+		showLifeStr = std::to_wstring(life);
+		showLife->setText(showLifeStr.c_str(), showLifeStr.size());
+
+		int x = 1;
+		if (m_KeyboardTracker.lastState.IsKeyUp(Keyboard::W) && keyState.IsKeyDown(Keyboard::W)) {
+
+			x++;
+		}
+		if (x == 10) {
+			x++;
+		}
+
+		for (int i = 0; i < Maxn; i++)
+		{
+			LeftArrow[i]->setY(LeftArrow[i]->getY() + SPEED * dt);
+			RightArrow[i]->setY(RightArrow[i]->getY() + SPEED * dt);
+			UpArrow[i]->setY(UpArrow[i]->getY() + SPEED * dt);
+			DownArrow[i]->setY(DownArrow[i]->getY() + SPEED * dt);
+		}
+
+		end = time(NULL);
+
+		while (end - start == 2)
+		{
+			start = end;
+			int num = rand() % 4;
+			//int num = 2;
+
+			switch (num)
+			{
+			case 0:LeftArrow[site]->setX(325);
+				LeftArrow[site]->setY(0);
+				Sequence[s_site].shape = LeftArrow[site];
+				Sequence[s_site].value = 2;
+				addGraphic(LeftArrow[site]);
+				break;
+			case 1:RightArrow[site]->setX(725);
+				RightArrow[site]->setY(0);
+				Sequence[s_site].shape = RightArrow[site];
+				Sequence[s_site].value = 3;
+				addGraphic(RightArrow[site]);
+				break;
+			case 2:UpArrow[site]->setX(525);
+				UpArrow[site]->setY(0);
+				Sequence[s_site].shape = UpArrow[site];
+				Sequence[s_site].value = 0;
+				addGraphic(UpArrow[site]);
+				break;
+			case 3:DownArrow[site]->setX(925);
+				DownArrow[site]->setY(0);
+				Sequence[s_site].shape = DownArrow[site];
+				Sequence[s_site].value = 1;
+				addGraphic(DownArrow[site]);
+				break;
+			default:
+				break;
+			}
+
+			site = (site + 1) % Maxn;
+			s_site = (s_site + 1) % Max_Sequence;
+		}
+
+
+		//键盘输入
+		if ((s_site - now_site) % Max_Sequence >= 1)
+		{
+			if (Sequence[now_site].value == 0)
+			{
+				if (Sequence[now_site].shape->getY() - Upsite->getY() < EFFECT_AREA && Sequence[now_site].shape->getY() - Upsite->getY() > -EFFECT_AREA)
+				{
+					if (keyState.IsKeyDown(Keyboard::W))
+					{
+						removeGraphic(Sequence[now_site].shape);
+						now_site = (now_site + 1) % Max_Sequence;
+
+						score++;
+					}
+				}
+				else if (Sequence[now_site].shape->getY() - Upsite->getY() >= EFFECT_AREA)
+				{
+					removeGraphic(Sequence[now_site].shape);
+					now_site = (now_site + 1) % Max_Sequence;
+
+					life -= 10;
+				}
+			}
+			if (Sequence[now_site].value == 1)
+			{
+				if (Sequence[now_site].shape->getY() - Downsite->getY() < EFFECT_AREA && Sequence[now_site].shape->getY() - Downsite->getY() > -EFFECT_AREA)
+				{
+					if (keyState.IsKeyDown(Keyboard::S))
+					{
+						removeGraphic(Sequence[now_site].shape);
+						now_site = (now_site + 1) % Max_Sequence;
+
+						score++;
+					}
+				}
+				else if (Sequence[now_site].shape->getY() - Downsite->getY() >= EFFECT_AREA)
+				{
+					removeGraphic(Sequence[now_site].shape);
+					now_site = (now_site + 1) % Max_Sequence;
+
+					life -= 10;
+				}
+			}
+			if (Sequence[now_site].value == 2)
+			{
+				if (Sequence[now_site].shape->getY() - Leftsite->getY() < EFFECT_AREA && Sequence[now_site].shape->getY() - Leftsite->getY() > -EFFECT_AREA)
+				{
+					if (keyState.IsKeyDown(Keyboard::A))
+					{
+						removeGraphic(Sequence[now_site].shape);
+						now_site = (now_site + 1) % Max_Sequence;
+
+						score++;
+					}
+				}
+				else if (Sequence[now_site].shape->getY() - Leftsite->getY() >= EFFECT_AREA)
+				{
+					removeGraphic(Sequence[now_site].shape);
+					now_site = (now_site + 1) % Max_Sequence;
+
+					life -= 10;
+				}
+			}
+			if (Sequence[now_site].value == 3)
+			{
+				if (Sequence[now_site].shape->getY() - Rightsite->getY() < EFFECT_AREA && Sequence[now_site].shape->getY() - Rightsite->getY() > -EFFECT_AREA)
+				{
+					if (keyState.IsKeyDown(Keyboard::D))
+					{
+						removeGraphic(Sequence[now_site].shape);
+						now_site = (now_site + 1) % Max_Sequence;
+
+						score++;
+					}
+				}
+				else if (Sequence[now_site].shape->getY() - Rightsite->getY() >= EFFECT_AREA)
+				{
+					removeGraphic(Sequence[now_site].shape);
+					now_site = (now_site + 1) % Max_Sequence;
+
+					life -= 10;
+				}
+			}
+		}
+
+
+		// 最后更新键盘状态
+		m_KeyboardTracker.Update(keyState);
+		if (life < 0)
+		{
+			flag = false;
+		}
 	}
 	else {
-		temRect3->setOpacity(1.0f);
+		endGameRectangle->setX(350);
+		endGameRectangle->setY(300);
+		addGraphic(endGameRectangle);
+		sound->stop();
+		if (keyState.IsKeyDown(Keyboard::Q)) {
+			exit(0);
+		}
+		else if (keyState.IsKeyDown(Keyboard::R)) {
+			score = 0;
+			life = 100;
+			removeGraphic(endGameRectangle);
+			flag = true;
+			sound->play();
+		}
 	}
-
-	// 例子
-	// 键盘按下
-	if (keyState.IsKeyDown(Keyboard::W))
-		temRect3->setY(temRect3->getY() - 100.f * dt);
-	if (keyState.IsKeyDown(Keyboard::S))
-		temRect3->setY(temRect3->getY() + 100.f * dt);
-	if (keyState.IsKeyDown(Keyboard::A))
-		temRect3->setX(temRect3->getX() - 100.f * dt);
-	if (keyState.IsKeyDown(Keyboard::D))
-		temRect3->setX(temRect3->getX() + 100.f * dt);
-
-
-	// 最后更新键盘状态
-	m_KeyboardTracker.Update(keyState);
 }
